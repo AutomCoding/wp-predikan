ccf08b513fb9258815f87622b1f575955dc79034
Autom <40275136+AutomCoding@users.noreply.github.com>
2019-11-25 06:20:46 +0100

    Translate admin.php

--- templates/admin.php
+++ templates/admin.php
@@ -1,11 +1,16 @@
+<?php
+/**
+ * Text Domain: predikan
+ */
+?>
 <div class="wrap">
-	<h1>Inställningar för podcast</h1>
+	<h1><?php echo __("Podcast settings", "predikan"); ?></h1>
 	<form method="post" action="">
-		<h2>Kanal och metadata</h2>
-		<label for="predikan_description">Kanalbeskrivning</label>
+		<h2><?php echo __("Channel and metadata", "predikan"); ?></h2>
+		<label for="predikan_description"<?php echo __("Channel description", "predikan"); ?></label>
 		<textarea class="large-text" name="predikan_description" style="height:12em;"><?php print get_option("predikan_description", ""); ?></textarea>
-		<p class="description">Berätta med några meningar vad den här podcasten handlar om och vad lyssnaren kan förvänta sig.</p>
+		<p class="description"><?php echo __("Describe, in a few sentences, what this podcast is about and what the listener can expect.", "predikan"); ?></p>
 
-		<p class="submit"><input type="submit" name="podcast_settings_submit" class="button button-primary" value="Spara ändringar"></p>
+		<p class="submit"><input type="submit" name="podcast_settings_submit" class="button button-primary" value="<?php echo __("Save changes", "predikan"); ?>"></p>
 	</form>
 </div>
