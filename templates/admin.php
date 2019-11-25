dc62ca6803053ff8fd7574d06d692a1027238048
Autom <40275136+AutomCoding@users.noreply.github.com>
2019-11-25 16:52:30 +0100

    Missing > in HTML tag

--- templates/admin.php
+++ templates/admin.php
@@ -7,7 +7,7 @@
 	<h1><?php echo __("Podcast settings", "predikan"); ?></h1>
 	<form method="post" action="">
 		<h2><?php echo __("Channel and metadata", "predikan"); ?></h2>
-		<label for="predikan_description"<?php echo __("Channel description", "predikan"); ?></label>
+		<label for="predikan_description"><?php echo __("Channel description", "predikan"); ?></label>
 		<textarea class="large-text" name="predikan_description" style="height:12em;"><?php print get_option("predikan_description", ""); ?></textarea>
 		<p class="description"><?php echo __("Describe, in a few sentences, what this podcast is about and what the listener can expect.", "predikan"); ?></p>
 
