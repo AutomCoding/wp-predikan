8ef584b903e2984cddc78df515872f327045911d
Filip Bengtsson <me@autom.zone>
2024-01-24 17:21:09 +0100

    Date escaping

--- src/templates/admin.php
+++ src/templates/admin.php
@@ -12,23 +12,23 @@
 
 		<h2><?php esc_html_e( 'Podcast', 'predikan' ); ?></h2>
 		<label for="predikan_title"><?php esc_html_e( 'Channel title', 'predikan' ); ?></label><br />
-		<input type="text" name="predikan_title" value="<?php echo get_option( 'predikan_title', '' ); ?>"/><br /><br />
+		<input type="text" name="predikan_title" value="<?php echo esc_attr( get_option( 'predikan_title', '' ) ); ?>"/><br /><br />
 
 		<label for="predikan_author"><?php esc_html_e( 'Creator', 'predikan' ); ?></label><br />
-		<input type="text" name="predikan_author" value="<?php echo get_option( 'predikan_author', '' ); ?>"/>
+		<input type="text" name="predikan_author" value="<?php echo esc_attr( get_option( 'predikan_author', '' ) ); ?>"/>
 		<p class="description"><?php esc_html_e( 'Name of the creator used by iTunes, this will be publicly visible.', 'predikan' ); ?></p>
 
 		<label for="predikan_description"><?php esc_html_e( 'Channel description', 'predikan' ); ?></label>
-		<textarea class="large-text" name="predikan_description"><?php echo get_option( 'predikan_description', '' ); ?></textarea>
+		<textarea class="large-text" name="predikan_description"><?php echo esc_textarea( get_option( 'predikan_description', '' ) ); ?></textarea>
 		<p class="description"><?php esc_html_e( 'Describe, in a few sentences, what this podcast is about and what the listener can expect.', 'predikan' ); ?></p>
 
 		<h3><?php esc_html_e( 'Contact person', 'predikan' ); ?></h3>
 		<p class="description"><?php esc_html_e( 'Name and email address used by iTunes for ownership verification, this will be publicly accessible.', 'predikan' ); ?></p>
 		<label for="predikan_owner_name"><?php esc_html_e( 'Name', 'predikan' ); ?></label><br />
-		<input type="text" name="predikan_owner_name" value="<?php echo get_option( 'predikan_owner_name', '' ); ?>"/><br />
+		<input type="text" name="predikan_owner_name" value="<?php echo esc_attr( get_option( 'predikan_owner_name', '' ) ); ?>"/><br />
 
 		<label for="predikan_owner_email"><?php esc_html_e( 'Email address', 'predikan' ); ?></label><br />
-		<input type="text" name="predikan_owner_email" value="<?php echo get_option( 'predikan_owner_email', '' ); ?>"/><br />
+		<input type="text" name="predikan_owner_email" value="<?php echo esc_attr( get_option( 'predikan_owner_email', '' ) ); ?>"/><br />
 
 		<p class="submit"><input type="submit" name="podcast_settings_submit" class="button button-primary" value="<?php esc_attr_e( 'Save changes', 'predikan' ); ?>"></p>
 	</form>
--- src/templates/predikan-podcast.php
+++ src/templates/predikan-podcast.php
@@ -8,20 +8,20 @@ echo '<?xml version="1.0" encoding="' . get_option('blog_charset') . '"?>';
 
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
 	<channel>
-		<title><?php echo get_option( 'predikan_title' ); ?></title>
+		<title><?php echo esc_xml( get_option( 'predikan_title' ) ); ?></title>
 		<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
 		<link><?php bloginfo( 'url' ); ?></link>
-		<description><?php echo get_option( 'predikan_description' ); ?></description>
-		<itunes:author><?php echo get_option( 'predikan_author' ); ?></itunes:author>
+		<description><?php echo esc_xml( get_option( 'predikan_description' ) ); ?></description>
+		<itunes:author><?php echo esc_xml( get_option( 'predikan_author' ) ); ?></itunes:author>
 		<itunes:category text="Religion &amp; Spirituality">
 			<itunes:category text="Christianity"/>
 		</itunes:category>
-		<itunes:image href="<?php plugin_dir_url( __FILE__ ) . 'images/channel-logo.png'; ?>"/>
+		<itunes:image href="<?php echo plugin_dir_url( __FILE__ ) . 'images/channel-logo.png'; ?>"/>
 		<itunes:type>episodic</itunes:type>
 		<itunes:explicit>clean</itunes:explicit>
 		<itunes:owner>
-			<itunes:name><?php echo get_option( 'predikan_owner_name' ); ?></itunes:name>
-			<itunes:email><?php echo get_option( 'predikan_owner_email' ); ?></itunes:email>
+			<itunes:name><?php echo esc_xml( get_option( 'predikan_owner_name' ) ); ?></itunes:name>
+			<itunes:email><?php echo esc_xml( get_option( 'predikan_owner_email' ) ); ?></itunes:email>
 		</itunes:owner>
 		<lastBuildDate><?php echo mysql2date( 'D, d M Y H:i:s +0000', get_lastpostmodified( 'GMT' ), false ); ?></lastBuildDate>
 		<language><?php echo substr( get_locale(), 0, 2 ); ?></language>
@@ -30,12 +30,12 @@ echo '<?xml version="1.0" encoding="' . get_option('blog_charset') . '"?>';
 		<?php foreach( $episodes as $ep ): ?>
 
 		<item>
-			<title><?php echo $ep[ 'title' ]; ?></title>
+			<title><?php echo esc_xml( $ep[ 'title' ] ); ?></title>
 			<link><?php echo $ep[ 'permalink' ]; ?></link>
 			<description><![CDATA[<?php echo $ep[ 'title' ]; ?>]]></description>
 			<pubDate><?php echo date( 'r', $ep[ 'unix_time' ] ); ?></pubDate>
 			<content:encoded><![CDATA[<?php echo $ep[ 'title' ] . '<br/>' . $ep[ 'speakers_string' ] . ' ' . $ep[ 'date' ]; ?>]]></content:encoded>
-			<enclosure url="<?php echo $ep[ 'file' ]; ?>" length="<?php echo filesize( $ep[ 'file' ] ); ?>" type="audio/mpeg"/>
+			<enclosure url="<?php echo esc_attr( $ep[ 'file' ] ); ?>" length="<?php echo filesize( $ep[ 'file' ] ); ?>" type="audio/mpeg"/>
 		</item>
 		<?php endforeach; ?>
 
